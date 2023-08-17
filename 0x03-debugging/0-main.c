ac84890d80da4f33f5587afa7d8fa29cc7fd2b89
Ndjoli <armanndjoli97@gmail.com>
2023-08-17 02:41:43 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- *_main - Test function for positive or negative  
+ *main - test function taht prints if interger is positive or negative  
  * Return: 0
  */
 
 int main(void)
 {
 int i;
-im = 0;
+i= 0;
 positive_or_negative(i);
 return (0);
 }
